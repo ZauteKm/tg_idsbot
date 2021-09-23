75853798c0511550ff3722e8aef30b5c6c3f7f72
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-23 19:45:57 +0530

    Update groups.py

--- UseTGidBot/groups.py
+++ UseTGidBot/groups.py
@@ -17,8 +17,8 @@ from pyrogram import Client, filters
 
 # Groups Welcome
 @Client.on_message(filters.new_chat_members)
-async def welcome(idbot, msg):
-    bot_id = (await idbot.get_me())["id"]
+async def welcome(bot, msg):
+    bot_id = (await bot.get_me())["id"]
     if msg.new_chat_members[0].id == bot_id:
         await msg.reply(
             f"Thanks for adding me here! \n\nThis Group's ID is `{msg.chat.id}`"
