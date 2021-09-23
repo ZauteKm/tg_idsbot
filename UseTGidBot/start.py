06e823ed5aba21139daf0f1887c76ee34a8aa6a0
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-23 19:50:13 +0530

    Create start.py

---
+++ UseTGidBot/start.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Copyright (C) @ZauteKm
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from Data import Data
+from Config import Config
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup
+
+
+# Start Message
+@Client.on_message(filters.private & filters.incoming & filters.command("start"))
+async def start(bot, msg):
+    print("/start")
+    user = await bot.get_me()
+    mention = user["mention"]
+    await idbot.send_message(
+        msg.chat.id,
+        Data.START.format(msg.from_user.mention, mention, msg.from_user.id),
+        reply_markup=InlineKeyboardMarkup(Data.buttons),
+    )
