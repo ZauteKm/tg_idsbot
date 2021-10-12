f881bbfbf8e85ec762ed12f080be7049f74d1c03
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-10-12 15:44:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/UseTGidBot.git /UseTGidBot
-cd /UseTGidBot
+git clone https://github.com/ZauteKm/tg_idsbot.git /tg_idsbot
+cd /tg_idsbot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
